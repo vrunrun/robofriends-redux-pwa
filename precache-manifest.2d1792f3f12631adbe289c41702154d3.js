b3a1dc926c99161b4ca4cc6b4e8fa84496ab2ac2
kl <kl@debian>
2019-11-17 15:29:22 +0800

    Updates

---
+++ precache-manifest.2d1792f3f12631adbe289c41702154d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00b5d5cecf40a0481e0bd467b87501f",
+    "url": "/robofriends-redux-pwa/index.html"
+  },
+  {
+    "revision": "d9f49a4be85a9363960d",
+    "url": "/robofriends-redux-pwa/static/css/2.1e00258c.chunk.css"
+  },
+  {
+    "revision": "95319a577d8f3fce6b4f",
+    "url": "/robofriends-redux-pwa/static/css/main.379cc138.chunk.css"
+  },
+  {
+    "revision": "d9f49a4be85a9363960d",
+    "url": "/robofriends-redux-pwa/static/js/2.0325348e.chunk.js"
+  },
+  {
+    "revision": "95319a577d8f3fce6b4f",
+    "url": "/robofriends-redux-pwa/static/js/main.bed22f5b.chunk.js"
+  },
+  {
+    "revision": "046a056b69bd3617e974",
+    "url": "/robofriends-redux-pwa/static/js/runtime-main.a957d2e8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
